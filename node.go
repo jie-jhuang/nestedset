89dc4a5a845d07d1a8e4e6723110913a968db0e6
Jie <>
2020-04-15 15:15:46 +0800

    init

--- node.go
+++ node.go
@@ -2,33 +2,39 @@
 // Use of this source code is governed by a MIT
 // license that can be found in the LICENSE file.
 
-
 package nestedset
 
 // NodeInterface is the interface implemented by types that can be used by nodes in nested set
 type NodeInterface interface {
-	Type() string // Returns type of node
-	Name() string // Returns name of node
+	Type() string     // Returns type of node
+	Username() string // Returns name of node
+	Name() string     // Returns name of node
 
 	Id() int64    // Returns id of node
+	Aid() int64   // Returns id of node
+	Pid() int64   // Returns id of node
 	Level() int64 // Returns level of node
 	Left() int64  // Returns left of node
 	Right() int64 // Returns right of node
 
-	SetId(int64)      // Sets node id
-	SetName(string) // Sets node name
-	SetLevel(int64)   // Sets node level
-	SetLeft(int64)    // Sets node left
-	SetRight(int64)   // Sets node right
+	SetId(int64)        // Sets node id
+	SetUsername(string) // Sets node name
+	SetName(string)     // Sets node name
+	SetLevel(int64)     // Sets node level
+	SetLeft(int64)      // Sets node left
+	SetRight(int64)     // Sets node right
 }
 
 // Node represents generic node type with NodeInterface implementation
 type Node struct {
-	NodeId    int64    `json:"id"`
-	NodeName  string   `json:"node_name"`
-	NodeLevel int64    `json:"level"`
-	NodeLeft  int64    `json:"left"`
-	NodeRight int64    `json:"right"`
+	NodeId    int64  `json:"id"`
+	NodeName  string `json:"node_name"`
+	NodeLevel int64  `json:"level"`
+	NodeLeft  int64  `json:"left"`
+	NodeRight int64  `json:"right"`
+	Aid       int64  `json:"a_id"`
+	Pid       int64  `json:"p_id"`
+	Username  int64  `json:"username"`
 }
 
 // NewNode returns a new Node instance
@@ -44,13 +50,23 @@ func (n Node) Type() string {
 func (n Node) Name() string {
 	return n.NodeName
 }
+func (n Node) Username() string {
+	return n.Username
+}
 
 func (n Node) Id() int64 {
 	return n.NodeId
 }
 
-func (n Node) Level() int64 {
+func (n Node) Aid() int64 {
+	return n.Aid
+}
 
+func (n Node) Pid() int64 {
+	return n.Pid
+}
+
+func (n Node) Level() int64 {
 	return n.NodeLevel
 }
 
@@ -66,10 +82,19 @@ func (n *Node) SetId(id int64) {
 	n.NodeId = id
 }
 
+func (n *Node) SetAid(id int64) {
+	n.Aid = id
+}
+func (n *Node) SetPid(id int64) {
+	n.Pid = id
+}
+func (n *Node) SetUsername(name string) {
+	n.Username = name
+}
+
 func (n *Node) SetName(name string) {
 	n.NodeName = name
 }
-
 func (n *Node) SetLevel(level int64) {
 	n.NodeLevel = level
 }
